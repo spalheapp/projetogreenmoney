29817b7c1a8cdd90f0fe9f01ea621c43f991e7a2
Marcos <71572572+spalheapp@users.noreply.github.com>
2023-04-29 20:42:36 -0300

    Add files via upload

---
+++ scripts/scrollReveal.js
@@ -0,0 +1,17 @@
+window.sr = ScrollReveal({reset: true})
+
+ScrollReveal().reveal('nav', {
+    delay: 200
+})
+
+ScrollReveal().reveal('main', {
+    delay: 200
+})
+
+ScrollReveal().reveal('section', {
+    delay: 200
+})
+
+ScrollReveal().reveal('footer', {
+    delay: 200
+})
